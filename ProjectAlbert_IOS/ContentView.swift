d594f230d0d415b9186e0364936871467d6bcd25
Jonathan Ho <jonathanbho@outlook.com>
2020-03-16 13:27:12 -0400

    Initial Commit

---
+++ ProjectAlbert_IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectAlbert_IOS
+//
+//  Created by Jonathan Ho on 2020-03-16.
+//  Copyright © 2020 Jonathan Ho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
