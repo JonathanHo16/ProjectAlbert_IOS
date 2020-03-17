1ebc583ab239e949a238c40459645d72dab64c9d
Jonathan Ho <jonathanbho@outlook.com>
2020-03-17 16:11:38 -0400

    login page no call services

---
+++ ProjectAlbert_IOS/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  ProjectAlbert_IOS
+//
+//  Created by Jonathan Ho on 2020-03-16.
+//  Copyright © 2020 Jonathan Ho. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager: NSObject {
+
+}
